47c5c10f8bf482b95fe0b345ecbc72e312645808
zth-Felipe <felipe.solis.n@usach.cl>
2021-12-30 23:57:59 -0300

    Avance de Register

--- Codigo_Prolog.pl
+++ Codigo_Prolog.pl
@@ -89,31 +89,67 @@ getUsuariosId([_,_,_,_,Lista|_],Lista).
 % ---- TDA paradigmadoc
 % Formato: nombre paradigmadoc - fecha de paradigmadoc - lista de usuarios - lista de documentos - historial paradigmadoc.
 % Predicado TDA paradigmadoc
-paradigmadoc(NombreParadoc, FechaParadoc, [NombreParadoc, FechaParadoc, [], [], []]).
+paradigmadoc(NombreParadoc, FechaParadoc, [NombreParadoc, FechaParadoc, [], [], [], 0]).
+
+% Como constructor
+% paradigmadoc("paradigmadoc1", Fecha1, PARADOC1).
+% paradigmadoc("paradigmadoc2", Fecha2, PARADOC2).
+
+% Como pertenencia
+% paradigmadoc(_, _,["Doc1", Fecha1, [],[],[]]).
+% paradigmadoc(_, _,["Doc2", Fecha2, [],[],[]]).
+
+% Como selector
+getNombreParaDoc([Nombre|_],Nombre).
+getFechaParaDoc([_,Fecha|_],Fecha).
+getListaUsuParaDoc([_,_,ListU|_],ListU).
+getListaDocParaDoc([_,_,_,ListD|_],ListD).
+getListaHistorialParaDoc([_,_,_,_,ListH|_],ListH).
+getIdParaDoc([_,_,_,_,_,Id|_],Id).
+
+% Como modificador
+%reemplazar( _, _, [], []).
+%reemplazar( Antiguo, Reemplazo , [Antiguo|Resto], [Reemplazo|NuevoResto] ):- 
+%    reemplazar( Antiguo, Reemplazo, Resto, NuevoResto ).
+%reemplazar( Antiguo, Reemplazo, [Cabeza|Resto],[Cabeza|NuevoResto] ):- 
+%    Cabeza \= Antiguo, 
+%    reemplazar( Antiguo, Reemplazo, Resto, NuevoResto ).
+
+% --- MAIN --------------------------------------------------
+paradigmaDocsRegister(Sn1, Fecha, Username, Password, Sn2):-
+%    getNombreParaDoc(Sn1,Name),
+    getFechaParaDoc(Sn1,Fecha1),
+    getListaUsuParaDoc(Sn1,ListUsu1),
+%    getListaDocParaDoc(Sn1,ListDoc),
+    getListaHistorialParaDoc(Sn1,Historial1),
+    getIdParaDoc(Sn1,Id1),
+%    Id1 is Id2 + 1,
+    not(pertenece(ListUsu1, Username)),
+    usuario(Id1, Username, Password, [], Usuario),
+    insertarAlPrincipio(Usuario, ListUsu1, ListUsu2),
+    insertarAlPrincipio(Sn1, Historial1, Historial2),
+    reemplazar(ListUsu1,ListUsu2, Sn1, Sn1A),
+    reemplazar(Historial1,Historial2, Sn1A, Sn1B),
+    reemplazar(Fecha1,Fecha, Sn1B, Sn2),
+%    reemplazar(_,Id1, Sn1, Sn2),
+    true, !.
+%------------------------------------------------------------
 
 
+insertarAlPrincipio( Elemento, [], [Elemento] ).
+insertarAlPrincipio( Elemento, Lista, [Elemento|Lista] ).
 
+insertarSinDuplicados( [], Elemento, [Elemento] ).
+insertarSinDuplicados( [Elemento|Resto], Elemento, [Elemento|Resto] ) :- !.
+insertarSinDuplicados( [Cabeza|Resto], Elemento, [Cabeza|NuevoResto] ):-
+	insertarSinDuplicados( Resto, Elemento, NuevoResto ).
 
+insertarAlFinal( Elemento, [], [Elemento] ).
+insertarAlFinal( Elemento, [Cabeza|Resto], [Cabeza|Lista] ) :-
+        insertarAlFinal( Elemento, Resto, Lista ).
 
-
-
-
-
-
-
-
-
-% --- MAIN
-%paradigmaDocsRegister(Paradoc1, Fecha, Username, Password, Paradoc2).
-    
-
-
-
-
-
-
-
-
-
+pertenece([[_,Nombre,_,_]|_], Nombre):-!.
+pertenece([[_,_,_,_]|Resto], Nombre):-
+    pertenece(Resto,Nombre).
 
 
